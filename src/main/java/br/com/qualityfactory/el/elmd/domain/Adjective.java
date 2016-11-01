9eaf461d2246d320b786f808529ba1ab4dca5df5
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2016-11-01 20:39:41 -0200

    Criação dos artefatos para o model

--- src/main/java/br/com/qualityfactory/el/elmd/domain/Adjective.java
+++ src/main/java/br/com/qualityfactory/el/elmd/domain/Adjective.java
@@ -5,11 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Data
-@Entity(name = "adjective")
+@Entity
+@Table(name = "adjective")
 public class Adjective implements Model {
 
 	private static final long serialVersionUID = 1L;
