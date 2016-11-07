71c7a1b77dcf05c502dda71b00e81667f9b01972
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2016-11-07 21:59:07 -0200

    Alteração no Entity Manager para recuperar a conexão do container, remoção da anotação das classes no persistence.xml

--- src/main/java/br/com/qualityfactory/el/elmd/service/table/TableDefaultRepositoryImpl.java
+++ src/main/java/br/com/qualityfactory/el/elmd/service/table/TableDefaultRepositoryImpl.java
@@ -2,13 +2,33 @@ package br.com.qualityfactory.el.elmd.service.table;
 
 import java.util.Collection;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import br.com.qualityfactory.el.elmd.domain.Model;
 import br.com.qualityfactory.el.elmd.util.SQLFactory;
 
 class TableDefaultRepositoryImpl implements TableDefaultRepository {
+	
+	@PersistenceContext
+	private EntityManager manager; 
+	
 
 	@Override
 	public Collection<Model> listAllModels(Model model) {
-		return SQLFactory.getDataTable(model);
+		CriteriaQuery<Model> query = SQLFactory.getQuery(manager, model);
+		return doSelect(query, doFrom(model, query));
+	}
+
+	private Collection<Model> doSelect(CriteriaQuery<Model> query, Root<Model> variableRoot) {
+		query.select(variableRoot);
+		return (Collection<Model>) manager.createQuery(query).getResultList();
+	}
+
+	@SuppressWarnings("unchecked")
+	private Root<Model> doFrom(Model model, CriteriaQuery<Model> query) {
+		return (Root<Model>) query.from(model.getClass());
 	}
 }
--- src/main/java/br/com/qualityfactory/el/elmd/util/SQLFactory.java
+++ src/main/java/br/com/qualityfactory/el/elmd/util/SQLFactory.java
@@ -1,54 +1,25 @@
 package br.com.qualityfactory.el.elmd.util;
 
-import java.util.Collection;
-
 import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
 import br.com.qualityfactory.el.elmd.domain.Model;
 
 public class SQLFactory {
 
-	private static EntityManager manager;
 	private static CriteriaBuilder builder;
 
-	public static Collection<Model> getDataTable(Model model) {
-		CriteriaQuery<Model> query = getQuery(model);
-
-		return doSelect(query, doFrom(model, query));
-	}
-
 	@SuppressWarnings("unchecked")
-	private static CriteriaQuery<Model> getQuery(Model model) {
-		return (CriteriaQuery<Model>) getBuilder().createQuery(model.getClass());
+	public static CriteriaQuery<Model> getQuery(EntityManager manager, Model model) {
+		return (CriteriaQuery<Model>) getBuilder(manager).createQuery(model.getClass());
 	}
 
-	private static CriteriaBuilder getBuilder() {
+	private static CriteriaBuilder getBuilder(EntityManager manager) {
 		if (builder == null) {
-			builder = getManager().getCriteriaBuilder();
+			builder = manager.getCriteriaBuilder();
 		}
 
 		return builder;
-	}
-
-	@SuppressWarnings("unchecked")
-	private static Root<Model> doFrom(Model model, CriteriaQuery<Model> query) {
-		return (Root<Model>) query.from(model.getClass());
-	}
-
-	private static Collection<Model> doSelect(CriteriaQuery<Model> query, Root<Model> variableRoot) {
-		query.select(variableRoot);
-		return (Collection<Model>) getManager().createQuery(query).getResultList();
-	}
-
-	private static EntityManager getManager() {
-		if (manager == null) {
-			manager = Persistence.createEntityManagerFactory(BusinessContant.PERSISTENCE_UNIT).createEntityManager(); 
-		}
-		
-		return manager;
 	}
 }
